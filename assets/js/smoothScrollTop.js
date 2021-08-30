3cffeedaa5448b2ae310ec13eabc45f14f436e71
Kiko <kristiqnenchevv@gmail.com>
2021-08-30 13:03:39 +0300

    Create navigation menu and toggle

--- assets/js/smoothScrollTop.js
+++ assets/js/smoothScrollTop.js
@@ -1,2 +1,19 @@
 /*==================== SMOOTH SCROLL TOP ====================*/
-// This file will contain the scroll-to-top functionality
+
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-up');
+    if (this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop);
+
+// Smooth scroll behavior for all internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
