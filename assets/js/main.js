b3048cdcb85198bf4fc682a6ed84350876b7b175
Kiko <kristiqnenchevv@gmail.com>
2021-09-06 12:51:23 +0300

    Implement PDF generation functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,4 +83,23 @@ themeButton.addEventListener('click', () => {
 });
 
 /*==================== GENERATE PDF ====================*/
-// PDF generation will be added in a later commit
+let areaCv = document.getElementById('area-cv');
+let resumeButton = document.getElementById('resume-button');
+
+let opt = {
+    margin: 1,
+    filename: 'myResume.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 4 },
+    jsPDF: { format: 'a4', orientation: 'portrait' }
+};
+
+function generateResume() {
+    html2pdf(areaCv, opt);
+}
+
+resumeButton.addEventListener('click', () => {
+    scaleCv();
+    generateResume();
+    setTimeout(removeScale, 5000);
+});
