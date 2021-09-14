3fc77a342ae49ad940fa333ac9892f091fa74cdb
Kiko <kristiqnenchevv@gmail.com>
2021-09-14 12:55:15 +0300

    Final touches unify the css styles

--- assets/js/smoothScrollTop.js
+++ assets/js/smoothScrollTop.js
@@ -1,9 +1,3 @@
-/**
- * Implements smooth scrolling functionality for navigation links
- * Uses easing functions for a natural animation effect
- * 
- * @param {Event} event - The click event object
- */
 function smoothScroll(event) {
     event.preventDefault();
     const targetId = event.currentTarget.getAttribute('href') === '#' ? '.header' : event.currentTarget.getAttribute('href');
@@ -29,42 +23,25 @@ function smoothScroll(event) {
         }
     }
 
-    /**
-     * Cubic easing in/out function for smooth animation
-     * Accelerates in the first half and decelerates in the second half
-     * 
-     * @param {number} t - Current time
-     * @param {number} b - Start value
-     * @param {number} c - Change in value
-     * @param {number} d - Duration
-     * @returns {number} - Calculated position
-     */
     function easeInOutCubic(t, b, c, d) {
         t /= d / 2;
         if (t < 1) return c / 2 * t * t * t + b;
         t -= 2;
         return c / 2 * (t * t * t + 2) + b;
-    }
+    };
 
     requestAnimationFrame(step);
 }
 
-/**
- * Initialize smooth scrolling for navigation elements
- * Attaches event listeners to navigation links and scroll button
- */
-
-// Get all required elements for smooth scrolling
 let scrollUpBtn = document.getElementById('scroll-up');
 let allSections = document.querySelectorAll('.nav__item');
 let navlogo = document.querySelector('.nav__logo');
 
-// Attach event listeners to the elements
 scrollUpBtn.addEventListener('click', smoothScroll);
+
 navlogo.addEventListener('click', smoothScroll);
 
-// Add smooth scrolling to each navigation link
 allSections.forEach(section => {
-    let navLink = section.querySelector('.nav__link');
-    navLink.addEventListener('click', smoothScroll);
+    let some = section.querySelector('.nav__link');
+    some.addEventListener('click', smoothScroll);
 });
